8f6b195c0e2bab09fa20e9b0e8f81c80361fa3a5
samuelbenichou <samuelbenichou@hotmail.fr>
2020-07-16 22:45:11 +0300

    MyRecipesPage.vue

--- src/pages/MyRecipesPage.vue
+++ src/pages/MyRecipesPage.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="container">
-        <h1>my recipes page</h1>
+    <div>
+        <h1 class="title">{{ this.$root.store.username }}  Recipes</h1>
+
+        <RecipePreviewList v-if="myrecipes.length > 0" :numberInColumn="3" :recipes="myrecipes"></RecipePreviewList>
+        <RecipePreviewList class="title" v-else> {{ this.$root.store.username }} Recipes list is empty!!!</RecipePreviewList>
     </div>
 </template>
+
+<script>
+    import RecipePreviewList from "../components/RecipePreviewList";
+    export default {
+        components: {
+            RecipePreviewList
+        },
+        data() {
+            return {
+                myrecipes: [],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
